17698f575f118c72f5728eb8c2fd609b85a72ec1
mathieutranchida <mathieu.tranchida@gmail.com>
2021-01-01 17:49:56 +0100

    removed general errors

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import emailjs from "emailjs-com";
 import styled from "styled-components";
 import { gsap } from "gsap";
